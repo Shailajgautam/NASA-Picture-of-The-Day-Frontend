e441b47c6f65a728d17ffa3de273ae692fd6f5c6
Shailaj Gautam <shailajgautam1234@gmail.com>
2023-03-29 00:37:41 +0530

    fixes: oauth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect,  useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import DashBoard from '@/components/DashBoard';
 
@@ -8,48 +8,50 @@ export default function Index() {
   const router = useRouter();
   const [loggedIn, setLoggedIn] = useState(false);
 
+
   useEffect(() => {
-   //Receiving token from google OAuth through params
-    console.log(router.query)
+    if (!router.isReady) return;
     const { token, _ } = router.query;
-    console.log(token);
+    console.log(router.query);
     if (token) {
+      console.log("Token found from google");
       localStorage.setItem('token', token as string);
-    } 
+    } else {
+      //Get token from localStorage
+      const localToken = localStorage.getItem('token');
+      if (localToken) {
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/verify`, {
+          headers: {
+            Authorization: `Bearer ${localToken}`,
+          },
+        })
+          .then((res) => {
+            if (res.status === 200) {
+              setLoggedIn(true);
 
-    //Get token from localStorage
-    const localToken = localStorage.getItem('token');
-    if (localToken) {
-      fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/verify`, {
-        headers: {
-          Authorization: `Bearer ${localToken}`,
-        },
-      })
-        .then((res) => {
-          if (res.status === 200) {
-            setLoggedIn(true);
-
-          } else {
+            } else {
+              localStorage.removeItem('token');
+              router.push('/login');
+            }
+          })
+          .catch((err) => {
+            console.error(err);
             localStorage.removeItem('token');
             router.push('/login');
-          }
-        })
-        .catch((err) => {
-          console.error(err);
-          localStorage.removeItem('token');
-          router.push('/login');
-        });
+          });
+      } else {
+        router.push('/login');
+      }
     }
-  }, []);
-
+  }, [router.isReady]);
 
   function handleLogout() {
     localStorage.removeItem('token');
     router.push('/login');
   }
-  
-  
-  return  (
+
+
+  return (
     <div className="min-h-screen bg-cover bg-center text-white" style={{ backgroundImage: "url('1.jpg')" }} >
       <div className='font-bold p-3'>
         <button onClick={handleLogout}>Logout</button>
